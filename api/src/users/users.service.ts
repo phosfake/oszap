683fd6f1c6beda68544990b9e25769e4dadf086b
Phosfake <phosfake@gmail.com>
2022-04-17 16:31:23 -0400

    clean up user service

--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -4,30 +4,14 @@ import {
   NotFoundException
 } from '@nestjs/common';
 import { DynamoDBService } from 'src/core/dynamodb/dynamodb.service';
-import { DeletionSuccess, FindResult } from 'src/core/typedorm/typedorm.model';
+import { DeletionSuccess } from 'src/core/typedorm/typedorm.model';
 import { User } from 'src/model/user/user.model';
 
 @Injectable()
 export class UsersService {
   constructor(private dynamoDb: DynamoDBService) {}
 
-  public get(id: string): Promise<User> {
-    const toGet = new User();
-    toGet.id = '6d9255da-3ee0-4748-b441-56bcb644b38b';
-    toGet.name = 'test zap';
-    throw 'not implemented';
-    //return from(this.dynamoDb.mapper.get<Zap>(toGet));
-  }
-
-  public async create(user: User): Promise<User> {
-    const existingUser = await this.dynamoDb.entityManager.findOne(User, user);
-    if (existingUser) {
-      throw new BadRequestException('Username taken. Try another username.');
-    }
-    return this.dynamoDb.entityManager.create<User>(user);
-  }
-
-  public async delete(id: string): Promise<DeletionSuccess> {
+  public async get(id: string): Promise<User> {
     const foundUsers = await this.dynamoDb.entityManager.find(
       User,
       {
@@ -41,16 +25,26 @@ export class UsersService {
     const userCount = foundUsers.items.length;
     if (userCount > 1) {
       throw new BadRequestException(
-        `Failed to delete: More than 1 user found with id ${id}.`
+        `Failed to get: More than 1 user found with id ${id}.`
       );
     }
     if (userCount <= 0) {
-      throw new NotFoundException(
-        `Failed to delete: No user found with id ${id}`
-      );
+      throw new NotFoundException(`Failed to get: No user found with id ${id}`);
     }
 
-    const user = foundUsers.items[0];
+    return Promise.resolve(foundUsers.items[0]);
+  }
+
+  public async create(user: User): Promise<User> {
+    const existingUser = await this.dynamoDb.entityManager.findOne(User, user);
+    if (existingUser) {
+      throw new BadRequestException('Username taken. Try another username.');
+    }
+    return this.dynamoDb.entityManager.create<User>(user);
+  }
+
+  public async delete(id: string): Promise<DeletionSuccess> {
+    const user = await this.get(id);
 
     return this.dynamoDb.entityManager.delete(User, {
       name: user.name
